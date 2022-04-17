21fbf097ced9e9ae48be071cc9de4375ba237821
Andrewy2416 <andrewy2416@gmail.com>
2022-04-17 05:23:37 -0700

    finished header component

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -4,7 +4,7 @@ import {AiFillGithub} from 'react-icons/ai'
 
 const HeaderSocials = () => {
   return (
-    <div className='header__socials'>
+    <div className="header__socials">
         <a href="https://www.linkedin.com/in/andrewy2416/" target="_black"><AiOutlineLinkedin/></a>
         <a href="https://github.com/Andrewy2416" target="_black"><AiFillGithub/></a>
     </div>
