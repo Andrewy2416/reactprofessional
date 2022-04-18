97cfcad3e17b5239a0d5f13f0bf9b597314d2571
Andrewy2416 <andrewy2416@gmail.com>
2022-04-17 19:38:50 -0700

    added more ustStates

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -13,9 +13,9 @@ const Nav = () => {
     <nav>
       <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome/></a>
       <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''} ><AiOutlineUser/></a>
-      <a href="#experience"><BsBook/></a>
-      <a href="#portfolio"><AiOutlineFolderOpen/></a>
-      <a href="#contact"><AiOutlineMessage/></a>
+      <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BsBook/></a>
+      <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><AiOutlineFolderOpen/></a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><AiOutlineMessage/></a>
     </nav>
   )
 }
