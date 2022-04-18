ecfc6443fd21f17d6a10e61fd7a661d92a7a2457
Andrewy2416 <andrewy2416@gmail.com>
2022-04-17 17:52:50 -0700

    added social buttons

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
-
 import './nav.css'
+import {AiOutlineHome} from 'react-icons/ai'
+import {AiOutlineUser} from 'react-icons/ai'
+import {BsBook} from 'react-icons/bs'
+import {AiOutlineFolderOpen} from 'react-icons/ai'
+import {AiOutlineMessage} from 'react-icons/ai'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#"><AiOutlineHome/></a>
+      <a href="#about"><AiOutlineUser/></a>
+      <a href="#experience"><BsBook/></a>
+      <a href="#portfolio"><AiOutlineFolderOpen/></a>
+      <a href="#contact"><AiOutlineMessage/></a>
+    </nav>
   )
 }
 
