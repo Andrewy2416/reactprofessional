6bfb443fb21ebd442e97010c06698132856613a9
Andrewy2416 <andrewy2416@gmail.com>
2022-05-11 02:37:34 -0700

    setup jsx and most of the css for articles and reviews

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -8,8 +8,7 @@ import {AiFillProject} from 'react-icons/ai'
 const About = () => {
   return (
     <section id='about'>
-      <h5>Get to Know</h5>
-      <h2>About Me</h2>
+      <h2>Get to Know Me</h2>
 
       <div className="container about__container">
         <div className="about__me">
