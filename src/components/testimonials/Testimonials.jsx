628477a9b330ef0b1cef3b0629027c9747cd6fd8
Andrewy2416 <andrewy2416@gmail.com>
2022-05-11 03:15:08 -0700

    added contact jsx and css

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -24,20 +24,7 @@ const Testimonials = () => {
            </small>
 
        </article>
-       <h2>Articles</h2>
-       <article className="testimonial">
-         <div className="client__avatar">
-           <img src={AVTR2} alt="Avatar Two"/>
-           </div>
-         <h5 className='client__name'>8BitDigi</h5>
-           <small className='client__review'>
-           Andrew was the bedrock for the projects I worked with him. His knowledge of the back-end was instrumental in 
-           making sure that our deployments were smooth, and his ability to problem solve around the issues we faced on 
-           the fly has enabled us to meet our deadlines for our major projects. I would highly recommend him for any full-stack 
-           development role that he would be applying to.
-           </small>
-
-       </article>
+       
      </div>
     </section>
   )
